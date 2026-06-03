2ab302c2e96831c3d09280ff601e8edfb88e55bf
Rafi Arif Putra <rafi140906@gmail.com>
2026-06-03 14:44:07 +0700

    add structs

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-
+struct Inventory {
+    int id;
+    char name[50];
+    char category[50];
+    int quantity;
+    char location[50];
+    char status[50]; // bisa juga int, misal 0 untuk tersedia, 1 untuk habis, dll
+};
 
 int main(){
     printf("Hello World\n");
